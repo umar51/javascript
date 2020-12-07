ce897bb47abb55156f295a196c773f17ca6f8eaa
umar51 <66181738+umar51@users.noreply.github.com>
2020-12-07 15:18:12 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("hello word");
+
+alert("again hello");
+
+window.alert("hello word");
+
+window.alert(23);
+alert(46);
+
+document.write("hello world from doccument.write")
---
+++ index2.js
@@ -0,0 +1,5 @@
+
+
+console.log("hello world");
+console.log(2+6);
+
