97fc02174dcda6e6c2996f8dfc8c11b4f5f38439
umar51 <66181738+umar51@users.noreply.github.com>
2020-12-21 20:25:51 +0500

    practice till video 60

---
+++ index4.js
@@ -0,0 +1,21 @@
+
+/* Multiline comment
+var x = "hello";
+
+console.log(x);*/ 
+
+//Singleline comment
+// x=true;
+// console.log(x);
+
+var _name="horse";
+var fee= 400;
+
+var $feepaid=true;
+console.log(typeof _name);
+console.log(typeof fee);
+console.log(typeof feepaid);
+
+feepaid = "yes";
+
+console.log(typeof $feepaid);
---
+++ index5.js
@@ -0,0 +1,18 @@
+
+a=78;
+b=87
+
+
+var sum = a + b;
+
+var sub = a - b;
+
+var multiply = a * b;
+
+var division = a/b;
+
+console.log(sum);console.log(sub);console.log(multiply);console.log(division);
+
+var increment = sum++ + multiply-- + division--
+
+console.log(increment+ "hello");  //string added to number makes result a string
